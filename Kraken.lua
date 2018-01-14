8098295d15128cd5744480247994784f2643343b
aaronk6 <aaronk6@users.noreply.github.com>
2018-01-14 18:59:54 +0100

    Added workaround for issue that occured after Kraken engine upgrade (closes #2), added support for currencies that cannot be exchanged to fiat money directly (closes #1), updated copyright year

--- Kraken.lua
+++ Kraken.lua
@@ -4,7 +4,7 @@
 -- Username: Kraken API Key
 -- Password: Kraken API Secret
 --
--- Copyright (c) 2017 aaronk6
+-- Copyright (c) 2018 aaronk6
 --
 -- Permission is hereby granted, free of charge, to any person obtaining a copy
 -- of this software and associated documentation files (the "Software"), to deal
@@ -25,7 +25,7 @@
 -- SOFTWARE.
 
 WebBanking{
-  version = 1.02,
+  version = 1.03,
   url = "https://api.kraken.com",
   description = "Fetch balances from Kraken API and list them as securities",
   services= { "Kraken Account" },
@@ -36,6 +36,7 @@ local apiSecret
 local apiVersion = 0
 local currency = "EUR" -- fixme: Don't hardcode
 local currencyName = "ZEUR" -- fixme: Don't hardcode
+local bitcoin = 'XXBT'
 local market = "Kraken"
 local accountName = "Balances"
 local accountNumber = "Main"
@@ -94,19 +95,27 @@ end
 
 function RefreshAccount (account, since)
   local name
-  local pair
+  local pair, bitcoinPair, targetCurrency, price
   local s = {}
 
   for key, value in pairs(balances) do
-    pair = getPairName(key)
+    pair, targetCurrency = getPairInfo(key)
     name = currencyNames[key] ~= nil and currencyNames[key] or key
     if prices[pair] ~= nil or key == currencyName then
+      price = prices[pair] ~= nil and prices[pair]["b"][1] or 1
+
+      -- If this currency pair cannot be changed to fiat directly, we get the price
+      -- in Bitcoin here and need to convert it to the correct fiat amount.
+      if targetCurrency == bitcoin then
+        price = price * prices[getPairInfo(bitcoin)]["b"][1]
+      end
+
       s[#s+1] = {
         name = name,
         market = market,
         currency = nil,
         quantity = value,
-        price = prices[pair] ~= nil and prices[pair]["b"][1] or 1
+        price = price
       }
     end
   end
@@ -134,8 +143,7 @@ function queryPrivate(method, request)
 
   connection = Connection()
   content = connection:request("POST", url .. path, postData, nil, headers)
-
-  json = JSON(content)
+  json = JSON(applyFillerWorkaround(content))
 
   return json:dictionary()["result"]
 end
@@ -148,11 +156,19 @@ function queryPublic(method, request)
   local path = string.format("/%s/public/%s", apiVersion, method)
   local postData = httpBuildQuery(request)
 
-  connection = Connection()
   content = connection:request("POST", url .. path, postData)
-  json = JSON(content)
+  json = JSON(applyFillerWorkaround(content))
 
   return json:dictionary()["result"]
+end
+
+function applyFillerWorkaround(content)
+  local fixVersion = '2.3.4'
+  if versionCompare(MM.productVersion, fixVersion) == -1 then
+    print("Adding filler to work around bug in product versions earlier than " .. fixVersion)
+    return '{"filler":"' .. string.rep('x', 1024) .. '",' .. string.sub(content, 2)
+  end
+  return content
 end
 
 function hex2str(hex)
@@ -179,9 +195,54 @@ function buildPairs(balances, assetPairs)
   return str.sub(str, 1, -2)
 end
 
-function getPairName(base)
+function getPairInfo(base)
   local opt1 = base .. currency
   local opt2 = base .. currencyName
+  local opt3 = base .. bitcoin
 
-  return assetPairs[opt1] ~= nil and opt1 or opt2
+  if assetPairs[opt1] ~= nil then return opt1, currency
+  elseif assetPairs[opt2] ~= nil then return opt2, currencyName
+  -- opt3: currency cannot be changed to fiat directly, only to Bitcoin (applies to Lumen, Dogecoin)
+  elseif assetPairs[opt3] then return opt3, bitcoin
+  end
+
+  return nil
+end
+
+function versionCompare(version1, version2)
+  -- based on https://helloacm.com/how-to-compare-version-numbers-in-c/
+
+  local v1 = split(version1, '.')
+  local v2 = split(version2, '.')
+
+  if #v1 ~= #v2 then error("version1 and version2 need to have the same number of fields") end
+
+  for i = 1, #v1 do
+    local n1 = tonumber(v1[i])
+    local n2 = tonumber(v2[i])
+    if n1 > n2 then return 1
+    elseif n1 < n2 then return -1
+    end
+  end
+
+  return 0
+end
+
+function split(str, delimiter)
+  -- from http://lua-users.org/wiki/SplitJoin
+  local t, ll
+  t = {}
+  ll = 0
+  if #str == 1 then return {str} end
+  while true do
+    l = string.find(str, delimiter, ll, true)
+    if l ~= nil then
+      table.insert(t, string.sub(str, ll, l-1))
+      ll = l + 1
+    else
+      table.insert(t, string.sub(str, ll))
+      break
+    end
+  end
+  return t
 end
