910fa1d5605e02f3cd740b963a65fbbde4c72182
aaronk6 <aaronk6@users.noreply.github.com>
2019-05-07 19:27:30 +0200

    Updated copyright to 2019

--- Kraken.lua
+++ Kraken.lua
@@ -4,7 +4,7 @@
 -- Username: Kraken API Key
 -- Password: Kraken API Secret
 --
--- Copyright (c) 2018 aaronk6
+-- Copyright (c) 2019 aaronk6
 --
 -- Permission is hereby granted, free of charge, to any person obtaining a copy
 -- of this software and associated documentation files (the "Software"), to deal
