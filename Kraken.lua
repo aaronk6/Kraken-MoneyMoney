6de359be9485853dce87162da8671c3c65819988
aaronk6 <aaronk6@users.noreply.github.com>
2024-01-31 20:42:05 +0100

    Happy new year 🎆

--- Kraken.lua
+++ Kraken.lua
@@ -4,7 +4,7 @@
 -- Username: Kraken API Key
 -- Password: Kraken API Secret
 --
--- Copyright (c) 2023 aaronk6, zacczakk
+-- Copyright (c) 2024 aaronk6, zacczakk
 --
 -- Permission is hereby granted, free of charge, to any person obtaining a copy
 -- of this software and associated documentation files (the "Software"), to deal
